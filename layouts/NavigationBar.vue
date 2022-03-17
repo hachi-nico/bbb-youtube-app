9e8ff81b00a3ed605dfce61ee82ce1bd853bcdcd
hachi-nico <nicokun48@gmail.com>
2022-03-17 21:29:32 +0700

    separate components

---
+++ layouts/NavigationBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar color="primary" fixed app clipped-left dark>
+    <v-app-bar-nav-icon @click.stop="$emit('c-on-click', drawer)" />
+    <v-toolbar-title v-text="title" />
+    <v-spacer />
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "NavigationBar",
+  props: ["title", "drawer"],
+  emits: ["c-on-click"],
+};
+</script>
---
+++ layouts/NavigationDrawer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- section identity -->
+    <div>
+      <v-avatar color="orange" size="66"
+        ><span class="white--text text-h5">36</span></v-avatar
+      >
+    </div>
+    <v-list>
+      <v-list-item
+        v-for="(item, i) in data"
+        :key="i"
+        :to="item.to"
+        router
+        exact
+      >
+        <v-list-item-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.title" />
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavigationDrawer",
+  props: ["data", "drawer"],
+};
+</script>
